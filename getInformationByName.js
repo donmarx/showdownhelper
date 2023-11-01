99f252c36c0671badaeb356ed6f6b9badc878c6c
Marcus Neves <marcus-neves@outlook.com>
2023-11-01 18:43:22 -0300

    refactor: if statement to handler fetch errors in the pokeAPI

--- getInformationByName.js
+++ getInformationByName.js
@@ -1,32 +1,22 @@
-console.log("loaded info by name")
-
 async function getInformationByName(pokemon){
     //standardizes the names for the api
-    console.log(pokemon)
-    var name = pokemon.trim().replace(" ", "-").toLowerCase()
-
-    var url = 'https://pokeapi.co/api/v2/pokemon/' + name;
-
-    var res = await fetch(url);
-    var info = await res.json();
-
-
-    var typesUrl = [];
-    for(let i=0; i < info["types"].length; i++){
-        typesUrl.push(info["types"][i]["type"]["url"]);
-    };
-
-    var PersonalTypeChart = await getDmgRelations(typesUrl);
-
-    return PersonalTypeChart;
-
-};
-
-function getPos(event){
-    let x = event.clientX;
-    let y = event.clientY;
-
-    const helper = document.getElementById('helper');
-    helper.style.top = y + "px";
-    helper.style.left = x + "px";
-};
+    const name = pokemon.trim().replace(" ", "-").toLowerCase();
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + name;
+    const res = await fetch(url);
+    if (res.status !== 200){
+        const status = res.status
+        let twoTimesWeak = [];
+        let fourTimesWeak = [];
+        let twoTimesResist = [];
+        let fourTimesResist = [];
+        let nodmg = [];
+        return [status,twoTimesWeak,fourTimesWeak,twoTimesResist,fourTimesResist,nodmg];
+    } else {
+        const info = await res.json();
+        const typesUrl = [];
+        for(let i=0; i < info["types"].length; i++){
+            typesUrl.push(info["types"][i]["type"]["url"]);
+        }
+        return await getDmgRelations(typesUrl);
+    }
+}
