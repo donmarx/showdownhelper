84461dd39f6b6928dc163c36c3ee99b191858b55
Marcus Neves <marcus-neves@outlook.com>
2023-11-01 18:42:35 -0300

    refactor: cleanup img elements, cleanup div appending, cleanup if statements, new strategy to observer mouseenter and mouseleave

--- app.js
+++ app.js
@@ -1,128 +1,94 @@
 typeImages = {
-    electric : `<img src=${chrome.runtime.getURL("images/Electric.png")} width= 40px"></img>`,
-    dark : `<img src=${chrome.runtime.getURL("images/Dark.png")} width= 40px"></img>`,
-    dragon : `<img src=${chrome.runtime.getURL("images/Dragon.png")} width= 40px"></img>`,
-    bug : `<img src=${chrome.runtime.getURL("images/Bug.png")} width= 40px"></img>`,
-    fairy : `<img src=${chrome.runtime.getURL("images/Fairy.png")} width= 40px"></img>`,
-    fighting : `<img src=${chrome.runtime.getURL("images/Fighting.png")} width= 40px"></img>`,
-    fire : `<img src=${chrome.runtime.getURL("images/Fire.png")} width= 40px"></img>`,
-    flying : `<img src=${chrome.runtime.getURL("images/Flying.png")} width= 40px"></img>`,
-    ghost : `<img src=${chrome.runtime.getURL("images/Ghost.png")} width= 40px"></img>`,
-    grass : `<img src=${chrome.runtime.getURL("images/Grass.png")} width= 40px"></img>`,
-    ground : `<img src=${chrome.runtime.getURL("images/Ground.png")} width= 40px"></img>`,
-    ice : `<img src=${chrome.runtime.getURL("images/Ice.png")} width= 40px"></img>`,
-    normal : `<img src=${chrome.runtime.getURL("images/Normal.png")} width= 40px"></img>`,
-    poison : `<img src=${chrome.runtime.getURL("images/Poison.png")} width= 40px"></img>`,
-    psychic : `<img src=${chrome.runtime.getURL("images/Psychic.png")} width= 40px"></img>`,
-    rock : `<img src=${chrome.runtime.getURL("images/Rock.png")} width= 40px"></img>`,
-    steel : `<img src=${chrome.runtime.getURL("images/Steel.png")} width= 40px"></img>`,
-    water : `<img src=${chrome.runtime.getURL("images/Water.png")} width= 40px"></img>`
+  electric: `<img src=${chrome.runtime.getURL("images/Electric.png")} alt="electric" width= 40px"/>`,
+  dark: `<img src=${chrome.runtime.getURL("images/Dark.png")} alt="dark" width= 40px"/>`,
+  dragon: `<img src=${chrome.runtime.getURL("images/Dragon.png")} alt="dragon" width= 40px"/>`,
+  bug: `<img src=${chrome.runtime.getURL("images/Bug.png")} alt="bug" width= 40px"/>`,
+  fairy: `<img src=${chrome.runtime.getURL("images/Fairy.png")} alt="fairy" width= 40px"/>`,
+  fighting: `<img src=${chrome.runtime.getURL("images/Fighting.png")} alt="fighting" width= 40px"/>`,
+  fire: `<img src=${chrome.runtime.getURL("images/Fire.png")} alt="fire" width= 40px"/>`,
+  flying: `<img src=${chrome.runtime.getURL("images/Flying.png")} alt="flying" width= 40px"/>`,
+  ghost: `<img src=${chrome.runtime.getURL("images/Ghost.png")} alt="ghost" width= 40px"/>`,
+  grass: `<img src=${chrome.runtime.getURL("images/Grass.png")} alt="grass" width= 40px"/>`,
+  ground: `<img src=${chrome.runtime.getURL("images/Ground.png")} alt="ground" width= 40px"/>`,
+  ice: `<img src=${chrome.runtime.getURL("images/Ice.png")} alt="ice" width= 40px"/>`,
+  normal: `<img src=${chrome.runtime.getURL("images/Normal.png")} alt="normal" width= 40px"/>`,
+  poison: `<img src=${chrome.runtime.getURL("images/Poison.png")} alt="poison" width= 40px"/>`,
+  psychic: `<img src=${chrome.runtime.getURL("images/Psychic.png")} alt="psychic" width= 40px"/>`,
+  rock: `<img src=${chrome.runtime.getURL("images/Rock.png")} alt="rock" width= 40px"/>`,
+  steel: `<img src=${chrome.runtime.getURL("images/Steel.png")} alt="steel" width= 40px"/>`,
+  water: `<img src=${chrome.runtime.getURL("images/Water.png")} alt="water" width= 40px"/>`
 }
 
-    function waitForElement(elementPath, callBack){
-        window.setTimeout(function(){
-          if($(elementPath).length){
-            callBack(elementPath, $(elementPath));
-          }else{
-            waitForElement(elementPath, callBack);
-          }
-        },500)
+async function handleHover() {
+  if (".statbar".length) {
+
+    // Normalize pokemon name before sending to the API
+    let pokemon = $(this).find("strong").text();
+    const level = $(this).find("small").text();
+    pokemon = pokemon.replace(level, "")
+
+    // Show loading to improve user experience
+    let newDiv = '<div id="helper"></div>';
+    $("body").append(newDiv);
+
+    // Call the API
+    const [
+      status,
+      twoTimesWeak,
+      fourTimesWeak,
+      twoTimesResist,
+      fourTimesResist,
+      nodmg
+    ] = await getInformationByName(pokemon);
+
+    // Check if the API call was successful
+    if (status !== 200){
+      let failedToLoadPokemonInfoDiv = '<div id="failedToLoadPokemonInfo" class="stat"><p>Could not load pokemon info!</p></div>';
+      $("#helper").append(failedToLoadPokemonInfoDiv);
+    } else {
+      // Populate helper div with stats
+      if (fourTimesWeak.length) {
+        let fourTimesWeakDiv = '<div id="fourTimesWeak" class="stat"><p>Four Times Weak:</p></div>';
+        $("#helper").append(fourTimesWeakDiv);
+        for (let i = 0; i < fourTimesWeak.length; i++) {
+          $("#fourTimesWeak").append(typeImages[fourTimesWeak[i]]);
+        }
       }
+      if (twoTimesWeak.length) {
+        let twoTimesWeakDiv = '<div id="twoTimesWeak" class="stat"><p>Two Times Weak:</p></div>';
+        $("#helper").append(twoTimesWeakDiv);
+        for (let i = 0; i < twoTimesWeak.length; i++) {
+          $("#twoTimesWeak").append(typeImages[twoTimesWeak[i]]);
+        }
+      }
+      if (twoTimesResist.length) {
+        let twoTimesResistDiv = '<div id="twoTimesResist" class="stat"><p>Two Times Resist:</p></div>';
+        $("#helper").append(twoTimesResistDiv);
+        for (let i = 0; i < twoTimesResist.length; i++) {
+          $("#twoTimesResist").append(typeImages[twoTimesResist[i]]);
+        }
+      }
+      if (fourTimesResist.length) {
+        let fourTimesResistDiv = '<div id="fourTimesResist" class="stat"><p>Four Times Resist:</p></div>';
+        $("#helper").append(fourTimesResistDiv);
+        for (let i = 0; i < fourTimesResist.length; i++) {
+          $("#fourTimesResist").append(typeImages[fourTimesResist[i]]);
+        }
+      }
+      if (nodmg.length) {
+        let nodmgDiv = '<div id="nodmg" class="stat"><p>Immune To:</p></div>';
+        $("#helper").append(nodmgDiv);
+        for (let i = 0; i < nodmg.length; i++) {
+          $("#nodmg").append(typeImages[nodmg[i]]);
+        }
+      }
+    }
+  } else {
+    console.log("DOM not loaded yet")
+  }
+}
 
-      const callback = async function(mutationsList, observer){
-        $(".statbar").on("mouseenter",async function buildHelper(event) {
-    
-            console.log("hovered");
-    
-            var pokemon = $(this).find("strong").text();
-            var level = $(this).find("small").text();
-    
-            console.log(pokemon)
-    
-            pokemon = pokemon.replace(level, "")
-    
-            var [twoTimesWeak, 
-                 fourTimesWeak, 
-                 twoTimesResist, 
-                 fourTimesResist, 
-                 nodmg] = await getInformationByName(pokemon);
-            
-            //style top and left
-            var newDiv = '<div id="helper"></div>';
-    
-            $("body").append(newDiv);
-            
-            if(fourTimesWeak.length>0){
-                var fourTimesWeakDiv = '<div id="fourTimesWeak" class="stat"><p>Four Times Weak:</p></div>'
-                $("#helper").append(fourTimesWeakDiv);
-    
-                for(let i=0; i<fourTimesWeak.length; i++){
-                    $("#fourTimesWeak").append(typeImages[fourTimesWeak[i]]);
-                };
-            };
-    
-            if(twoTimesWeak.length>0){
-                var twoTimesWeakDiv = '<div id="twoTimesWeak" class="stat"><p>Two Times Weak:</p></div>'
-                $("#helper").append(twoTimesWeakDiv);
-    
-                for(let i=0; i<twoTimesWeak.length; i++){
-                    $("#twoTimesWeak").append(typeImages[twoTimesWeak[i]]);
-                };
-            };
-
-            if(twoTimesResist.length>0){
-                var twoTimesResistDiv = '<div id="twoTimesResist" class="stat"><p>Two Times Resist:</p></div>'
-                $("#helper").append(twoTimesResistDiv);
-    
-                for(let i=0; i<twoTimesResist.length; i++){
-                    $("#twoTimesResist").append(typeImages[twoTimesResist[i]]);
-                };
-            };
-    
-            if(fourTimesResist.length>0){
-                var fourTimesResistDiv = '<div id="fourTimesResist" class="stat"><p>Four Times Resist:</p></div>'
-                $("#helper").append(fourTimesResistDiv);
-    
-                for(let i=0; i<fourTimesResist.length; i++){
-                    $("#fourTimesResist").append(typeImages[fourTimesResist[i]]);
-                };
-            };
-    
-    
-            if(nodmg.length>0){
-                var nodmgDiv = '<div id="nodmg" class="stat"><p>Immune To:</p></div>'
-                $("#helper").append(nodmgDiv);
-    
-                for(let i=0; i<nodmg.length; i++){
-                    $("#nodmg").append(typeImages[nodmg[i]]);
-                };
-            };
-    
-            getPos(event);
-    
-        });
-        
-         $(".statbar").on("mouseleave",async function(event){
-            $("#helper").remove();
-        }); 
-    
-    };
-
-      waitForElement(".statbar",function(){
-        callback();
-        const config = { childList: true, characterData: true, attributes: true, subtree: true }
-        let observer1 = new MutationObserver(callback);
-        let observer2 = new MutationObserver(callback);
-        const targetNode = $(".statbar");
-        console.log(targetNode)
-        console.log(targetNode[0])
-        console.log(targetNode[1])
-
-            observer1.observe(targetNode[0], config);
-            observer2.observe(targetNode[1], config);
-
-        
-        console.log("done");
-    });
-
-        
+$(document).on("mouseenter", ".statbar", handleHover);
+$(document).on("mouseleave", ".statbar", function() {
+    $("#helper").remove();
+});
